bbffaa3ea7cb44bd0a5caab2d5cc9fc4770e636b
b0kch01 <nath49ers@gmail.com>
2019-08-30 16:52:32 -0700

    update

---
+++ Methods Headers BasicMath/Calculate Library/src/Calculate.java
@@ -0,0 +1,10 @@
+// Nathan Choi
+// August 2019
+// APCS3
+// Calculate.java --> Library of Math Functions
+
+public class Calculate {
+    public static int square(int number){
+    	return number*number;
+    }
+}
