f4fd23778dcaf0e4033ca673796433321ea609d2
b0kch01 <nath49ers@gmail.com>
2019-09-24 10:48:36 -0700

    More stuff

--- QuadraticDescriber/src/Client/QuadraticClient.java
+++ QuadraticDescriber/src/Client/QuadraticClient.java
@@ -1,7 +1,21 @@
-package Methods;
+package Client;
+
+import Methods.*;
+import java.util.Scanner;
 
 public class QuadraticClient {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to QuadraticDescriber");
+        System.out.println("Provide coefficents for a b c");
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        System.out.println("Description of the graph:");
+        System.out.println(Quadratic.quadrDescriber(a, b, c));
+
     }
 }
--- QuadraticDescriber/src/Methods/Quadratic.java
+++ QuadraticDescriber/src/Methods/Quadratic.java
@@ -1,7 +1,22 @@
 package Methods;
 
 public class Quadratic {
-    public static void test() {
-        System.out.println("Hello from Quadratic");
+    public static String quadrDescriber(double a, double b, double c) {
+    	String a_str = "";
+    	String b_str = "";
+    	String c_str = "";
+    	String var_name = "x";
+
+    	if (a < 0) a_str += "-";
+    	if (b < 0) b_str += "-";
+    	if (c < 0) c_str += "-";
+
+    	return a + var_name + "^2" + b + var_name + c;
     }
+
+	public static double absValue(double number) {
+		if (number < 0)
+			return -number;
+		return number;
+	}
 }
