6af78a0e2d69c2a19f8447fa0fa0dfcf96a9960e
b0kch01 <nath49ers@gmail.com>
2019-09-24 17:18:53 -0700

    QuadDresbr Complete

--- QuadraticDescriber/src/Client/QuadraticClient.java
+++ QuadraticDescriber/src/Client/QuadraticClient.java
@@ -8,13 +8,17 @@ public class QuadraticClient {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to QuadraticDescriber");
-        System.out.println("Provide coefficents for a b c");
+        System.out.println("\nProvide coefficents for a b c");
+        System.out.println("--------------------");
 
+        System.out.print("a:");
         double a = sc.nextDouble();
+        System.out.print("b:");
         double b = sc.nextDouble();
+        System.out.print("c:");
         double c = sc.nextDouble();
 
-        System.out.println("Description of the graph:");
+        System.out.println("\nDescription of the graph:");
         System.out.println(Quadratic.quadrDescriber(a, b, c));
 
     }
--- QuadraticDescriber/src/Methods/Quadratic.java
+++ QuadraticDescriber/src/Methods/Quadratic.java
@@ -5,18 +5,39 @@ public class Quadratic {
     	String a_str = "";
     	String b_str = "";
     	String c_str = "";
-    	String var_name = "x";
+    	int a_int = (int) a;
+    	int b_int = (int) b;
+    	int c_int = (int) c;
 
-    	if (a < 0) a_str += "-";
-    	if (b < 0) b_str += "-";
-    	if (c < 0) c_str += "-";
+    	a_str += (a < 0) ? "-"  : "";
+        
+        if (absValue(a) == 1.0)
+            a_str += "";
+        else if (((int) a) / a == 1.0)
+            a_str += absValue(a_int);
+        else
+            a_str += absValue(a);
 
-    	return a + var_name + "^2" + b + var_name + c;
+    	b_str += (b < 0) ? " - " : " + ";
+    	if (((int) b) / b == 1.0)
+            b_str += absValue(b_int);
+        else
+            b_str += absValue(b);
+
+    	c_str += (c < 0) ? " - " : " + ";
+    	if (((int) c) / c == 1.0)
+            c_str += absValue(c_int);
+        else
+            c_str += absValue(c);
+
+    	return a_str + "x^2" + b_str + "x" + c_str;
+    }
+
+    public static int absValue(int number) {
+        return (number < 0) ? -number : number;
     }
 
 	public static double absValue(double number) {
-		if (number < 0)
-			return -number;
-		return number;
+        return (number < 0) ? -number : number;
 	}
 }
