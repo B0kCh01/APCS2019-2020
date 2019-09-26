a799a3121e5b5a8ce5620fce6b3609c471336620
b0kch01 <nath49ers@gmail.com>
2019-09-25 19:29:26 -0700

    Finished

--- QuadraticDescriber/src/Client/QuadraticClient.java
+++ QuadraticDescriber/src/Client/QuadraticClient.java
@@ -5,22 +5,84 @@ import Methods.*;
 import java.util.Scanner;
 
 public class QuadraticClient {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean exit = false;
+        while (!exit) {
+            runDescriber();
+            if (!menuSelect().equals("1"))
+                exit = true;
+        }
+        System.out.println("See ya!");
+    }
+
+    // "Renders" Menu selection
+    private static String menuSelect() {
+        scrollUp();
+        System.out.println("--------------------");
+        System.out.println("What would you like to do?");
+        System.out.print("[1]: New Quadratic\n[2]: Exit\n\nChoice: ");
+
+        String choice;
+        boolean pass = false;
+        do {
+            choice = sc.nextLine();
+            if (choice.equals("1") || choice.equals("2")) {
+                pass = true;
+            } else {
+                scrollUp();
+                System.out.println("What you gave me: " + choice);
+                System.out.println("--------------------");
+                System.out.println("Enter [1] or [2] only please.");
+                System.out.print("\n[1]: New Quadratic\n[2]: Exit\n\nChoice: ");
+            }
+        } while (!pass);
+        return choice;
+    }
+
+    // Main function
+    private static void runDescriber() {
+        scrollUp();
         System.out.println("Welcome to QuadraticDescriber");
-        System.out.println("\nProvide coefficents for a b c");
+        System.out.println("\nProvide coefficients for a b c (Enter any real number)");
         System.out.println("--------------------");
 
-        System.out.print("a:");
-        double a = sc.nextDouble();
-        System.out.print("b:");
-        double b = sc.nextDouble();
-        System.out.print("c:");
-        double c = sc.nextDouble();
+        double a = getDouble("a: ");
+        double b = getDouble("b: ");
+        double c = getDouble("c: ");
 
+        scrollUp();
         System.out.println("\nDescription of the graph:");
         System.out.println(Quadratic.quadrDescriber(a, b, c));
+        System.out.println(Quadratic.quadrDetails(a, b, c));
 
+        System.out.println("\nPress [Enter] to continue");
+        sc.nextLine();
+    }
+
+    // "Jank" substitute for "cls" or "clear"
+    private static void scrollUp() {
+        for (int i = 0; i < 20; i++)
+            System.out.println("\n");
+    }
+
+    // Method to get user input (securely)
+    private static double getDouble(String prompt) {
+        double proposed = 0;
+        boolean exit = false;
+        while (!exit) {
+            // I'm very sorry. This is probably the best way
+            try {
+                System.out.print(prompt);
+                String test = sc.nextLine();
+                proposed = Double.parseDouble(test);
+                exit = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Enter a number please!");
+            }
+        }
+        return proposed;
     }
 }
--- QuadraticDescriber/src/Methods/Quadratic.java
+++ QuadraticDescriber/src/Methods/Quadratic.java
@@ -6,23 +6,24 @@
 package Methods;
 
 public class Quadratic {
+    // Returns formatted quadratic equation from a, b, and c
     public static String quadrDescriber(double a, double b, double c) {
-        // Initalize all of the variables that will be used
-    	String a_str = "";
-    	String b_str = "";
-    	String c_str = "";
-    	int a_int = (int) a;
-    	int b_int = (int) b;
-    	int c_int = (int) c;
+        // Initialize all of the variables that will be used
+        String a_str = "";
+        String b_str = "";
+        String c_str = "";
+        int a_int = (int) a;
+        int b_int = (int) b;
+        int c_int = (int) c;
 
-        /** 
+        /*
             Manage the space between negatives / positives
-            Used turnary operators as a quick way to do it
-        **/
-    	a_str += (a < 0) ? "-"   : "";
+            Used ternary operators as a quick way to do it
+        */
+        a_str += (a < 0) ? "-" : "";
         b_str += (b < 0) ? " - " : " + ";
         c_str += (c < 0) ? " - " : " + ";
-        
+
         // Code below eliminates the ugly trailing zeros shown in doubles
         if (absValue(a) == 1.0)
             a_str += "";
@@ -30,29 +31,93 @@ public class Quadratic {
             a_str += absValue(a_int);
         else
             a_str += absValue(a);
-    	
-    	if (((int) b) / b == 1.0) // if b is an integer
+
+        if (((int) b) / b == 1.0) // if b is an integer
             b_str += absValue(b_int);
         else
             b_str += absValue(b);
 
-    	if (((int) c) / c == 1.0) // if c is an integer
+        if (((int) c) / c == 1.0) // if c is an integer
             c_str += absValue(c_int);
         else
             c_str += absValue(c);
 
-        // Constructs and conacatenates the Strings altogther
-    	return a_str + "x^2" + b_str + "x" + c_str;
+        // Constructs and concatenates the Strings altogether
+        return a_str + "x^2" + b_str + "x" + c_str;
     }
 
+    // Returns details about a quadratic equation from a, b, and c
+    public static String quadrDetails(double a, double b, double c) {
+        String output = "";
+        double h = -b / (2 * a);
+        double k = h * h * a + h * b + c;
+
+        output += (a > 0) ? "\nOpens: Upward\n" : "\nOpens: Downward\n"; // Positive / Negative
+        output += "Axis of symmetry: " + round2(h); // Symmetry
+        output += "\nVertex: (" + round2(h) + ", " + round2(k) + ")";
+        output += "\nx-intercepts: " + getRoots(a, b, c);
+        output += "\ny-intercept: " + c;
+        return output;
+    }
+
+    // ~DEPENDENCIES
+
+    private static String getRoots(double a, double b, double c) {
+        if (discriminant(a, b, c) < 0)
+            return "no real roots";
+
+        double root1 = round2((-b + sqrt(discriminant(a, b, c))) / (2 * a));
+        double root2 = round2((-b - sqrt(discriminant(a, b, c))) / (2 * a));
+
+        if (root1 == root2)
+            return "" + root1;
+        else
+            return min(root1, root2) + " and " + max(root1, root2);
+    }
+
+    // Returns the absolute value of the input
     private static int absValue(int number) {
-        // Turnary operator to return abs value
+        // Ternary operator to return abs value
         return (number < 0) ? -number : number;
     }
 
     // Overloaded for doubles
-	private static double absValue(double number) {
-        // Turnary operator to return abs value
+    private static double absValue(double number) {
+        // Ternary operator to return abs value
         return (number < 0) ? -number : number;
-	}
+    }
+
+    // Rounds input to the nearest hundredths
+    private static double round2(double number) {
+        if (number < 0)
+            return ((int) (number * 100 - 0.5)) / 100.0;
+        return ((int) (number * 100 + 0.5)) / 100.0;
+    }
+
+    // Check for real roots and root calculation
+    private static double discriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
+    }
+
+    // Square root of a number up to a certain precision
+    private static double sqrt(double number) {
+        if (number < 0)
+            throw new IllegalArgumentException(number + " - cannot find square root of a negative number");
+
+        double root = number / 2;
+        do {
+            root = 0.5 * (number / root + root);
+        } while (absValue(number - root * root) > 0.25);
+        return round2(root);
+    }
+
+    // Returns the lesser of two values
+    private static double min(double a, double b) {
+        return a <= b ? a : b;
+    }
+
+    // Returns the greater of two values
+    private static double max(double a, double b) {
+        return a >= b ? a : b;
+    }
 }
