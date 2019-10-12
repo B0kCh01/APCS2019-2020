8903ce6be8028e0c98519a23d4189e00a9860dd4
b0kch01 <nath49ers@gmail.com>
2019-10-12 10:43:11 -0700

    Robust Hourglass

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,37 +1,69 @@
+import java.util.Scanner;
+
 public class Hourglass {
     public static void main(String[] args) {
-        print("| ", "");
-        printThis(10, "\" ");
-        print("|", "\n");
+        // Create new Scanner Object and print welcome message
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! Welcome to Hourglass.java!\n");
+        System.out.println("Enter the width of the hourglass, and I'll make one for ya! ");
 
-        for (int row = 1; row <= 4; row++) {
-            printThis(row, "  ");
-            print("\\ ", "");
-            printThis(-2*row+10, ": ");
-            print("/", "\n");
+        // Prompt user for the width of the hourglass
+        System.out.print("Your length (I love big numbers): ");
+        int width = sc.nextInt();
+
+        if (width > 300) {
+            System.out.println(
+                    "\nOh my! That's a really big number. \n[ Enter ] \"yes!\" if you are sure.");
+            System.out.print("> ");
+            sc.nextLine();
+            String sure = sc.nextLine();
+            if (sure.equals("yes!")) {
+                // Build and print the hour glass with the given width
+                buildHGlass(width);
+            } else {
+                System.out.println("\nYou weren't sure.");
+            }
+        } else {
+            buildHGlass(width);
+            System.out.println("\nTa Da!");
         }
-
-        printThis(9, " ");
-        System.out.println("|   |");
-
-        for (int row = 4; row >= 1; row--) {
-            printThis(row, "  ");
-            print("/ ", "");
-            printThis(-2*row+10, ": ");
-            print("\\ ", "\n");
-        }
-
-        print("| ", "");
-        printThis(10, "\" ");
-        print("|", "\n");
     }
 
-    private static void print(String string, String end){
-        System.out.print(string + end);
+    private static void buildHGlass(int width) {
+        buildTop(width);
+        repeatThis(width - 1, " ");
+        System.out.println("|   |"); // Size of middle is fixed
+        buildBottom(width);
     }
 
-    private static void printThis(int num, String character) {
-        for (int i = 0; i < num; i++)
+    private static void buildTop(int width){
+        System.out.print("\n\n| ");
+        repeatThis(width, "\" ");
+        System.out.println("|");
+
+        for (int row = 1; row <= (width - 2) / 2; row++) {
+            repeatThis(row, "  ");
+            System.out.print("\\ ");
+            repeatThis(-2*row+width, ": ");
+            System.out.println("/");
+        }
+    }
+
+    private static void buildBottom(int width) {
+        for (int row = (width - 2) / 2; row >= 1; row--) {
+            repeatThis(row, "  ");
+            System.out.print("/ ");
+            repeatThis(-2*row+width, ": ");
+            System.out.println("\\ ");
+        }
+
+        System.out.print("| ");
+        repeatThis(width, "\" ");
+        System.out.println("|");
+    }
+
+    private static void repeatThis(int times, String character) {
+        for (int i = 0; i < times; i++)
             System.out.print(character);
     }
 }
