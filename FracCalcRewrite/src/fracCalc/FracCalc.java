599079481903d01f816ace6fc0be627ef784871c
b0kch01 <nath49ers@gmail.com>
2020-01-13 15:23:04 -0800

    Fraction Object CLass

---
+++ FracCalcRewrite/src/fracCalc/FracCalc.java
@@ -0,0 +1,25 @@
+// Nathan Choi
+// APCS 3rd
+// January 13, 2020
+// Excepts expression and outputs answer (support fraction computation)
+
+package fracCalc;
+import java.util.*;
+
+public class FracCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("FracCalc by Nathan Choi (b0kch01)\nEnter \"quit\" to quit!\n\nInput: ");
+        for (String input = sc.nextLine();
+             !input.equalsIgnoreCase("quit");
+             System.out.print("\nInput: "), input = sc.nextLine())
+        {
+            System.out.println("Answer: " + produceAnswer(input));
+        }
+    }
+
+    public static String produceAnswer(String input) {
+        Fraction fraction = new Fraction(1, 50);
+        return "";
+    }
+}
--- FracCalcRewrite/src/fracCalc/Fraction.java
+++ FracCalcRewrite/src/fracCalc/Fraction.java
@@ -1,4 +1,66 @@
 package fracCalc;
 
 public class Fraction {
+    private int whole;
+    private int numerator;
+    private int denominator;
+    private int negative;
+
+    public Fraction() {
+        numerator = 0;
+        whole = 0;
+        denominator = 1;
+        negative = 1;
+    }
+
+    public Fraction (int whole, int numerator, int denominator) {
+        this.whole = whole;
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.negative = 1;
+    }
+
+    private String parseFraction(String mixed) {
+        return "";
+
+    }
+
+    // Convert Fraction object to mixed number
+    public void toMixed() {
+        whole = numerator / denominator;
+        numerator = numerator % denominator;
+    }
+
+    // Convert Fraction object to Improper
+    private void toImproper() {
+        fixNeg();
+        numerator = whole*denominator + numerator;
+        whole = 0;
+    }
+
+    // Move all negatives to the negatives variable
+    private void fixNeg() {
+        int[] list = {whole, numerator, denominator};
+        for (int property : list)
+            negative *= property / abs(property);
+    }
+
+    // Absolute value
+    private int abs(int n) {
+        if (n < 0) return -n;
+        return n;
+    }
+
+    // Check if input String is a number (Thanks Nathan and Sebastian)
+    private boolean isInt(String string) {
+        if(string.contains("-"))
+            if (string.indexOf("-") != 0)
+                return false;
+            else
+                string = string.substring(1);
+        for (char c : string.toCharArray())
+            if (!Character.isDigit(c)) return false;
+        return true;
+    }
+
 }
