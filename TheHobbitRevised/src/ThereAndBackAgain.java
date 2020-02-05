127a787110529007776a017ba5086d6bc237b48a
b0kch01 <nath49ers@gmail.com>
2020-02-05 10:56:47 -0800

    1. Remove uneccessary things 2. Add super() in constructors that extend Traveler 3. Add PART1 Functionality

--- TheHobbitRevised/src/ThereAndBackAgain.java
+++ TheHobbitRevised/src/ThereAndBackAgain.java
@@ -1,33 +1,20 @@
 import java.util.ArrayList;
 
-public class ThereAndBackAgain 
-{
-
-	public static void main(String[] args) 
-	{
-		
+public class ThereAndBackAgain {
+	public static void main(String[] args) {
 		Hobbit frodo = new Hobbit("Frodo");
 		Hobbit sam = new Hobbit("Sam");
 		Dwarf gimli = new Dwarf("Gimli");
-		
-		// Create a traveling party called party1 by creating an array of Travelers 
+
+		// Create a traveling party called party1 by creating an array of Travelers
 		// and filling it with frodo, sam, and gimli
-		// Then, use a loop to make all travelers go a distance of 50 miles  
-		// Then, for each Traveler in the travelingParty, print their name and how far they've
-		//    traveled in miles.  (In the next piece, you'll do this in methods, but 
-		//    for a first pass, just do it in main and print to the console.)
-		// Expected output:  Frodo has traveled 50 miles.
-		//                   Sam has traveled 50 miles.
-		//                   Gimli has traveled 50 miles.
-		
-		
-		
-		
-		
-		
-		System.out.println();
-
+		Traveler[] party1 = {frodo, sam, gimli};
+		// Then, use a loop to make all travelers go a distance of 50 miles
+		for (Traveler traveler : party1)
+			traveler.travel(50);
+		// For each Traveler in the travelingParty, print their name and how far they've
+		// traveled in miles.
+		for (Traveler traveler : party1)
+			System.out.println(traveler.getName() + " has traveled " + traveler.getDistanceTraveled() + " miles.");
 	}
-
-	
 }
